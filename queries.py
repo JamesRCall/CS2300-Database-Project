9bc993a5b96e255b34e9bb63c5879f07ea2e0de9
James C <113481301+JamesRCall@users.noreply.github.com>
2024-04-24 16:22:40 -0500

    Create queries.py
    for reference

---
+++ queries.py
@@ -0,0 +1,65 @@
+import mysql.connector
+import random
+from enum import Enum
+
+db = mysql.connector.connect(
+    host= "localhost",
+    user= "USER",
+    password = "PASSWORD",
+    database = "DATABASE"
+
+)
+
+mycursor = db.cursor()
+
+#Login function
+def login():
+    found = 0
+    while found == 0:
+        id = input("Please enter your employee ID (enter 0 to quit): ")
+        if id == '0':
+            quit()
+        mycursor.execute("SELECT * FROM Employee WHERE employee_id = (%s)", (id,))
+        for x in mycursor:
+            found += 1
+        if found == 0:
+            print("That Employee ID does not exist. Please enter a valid ID or scram!")
+    
+    correct = 0
+    while correct == 0:
+        pswd = input("Please enter your password (enter 0 to quit): ")
+        if pswd == '0':
+            quit()
+        mycursor.execute("SELECT * FROM Employee WHERE employee_id = (%s) AND password = (%s)", (id, pswd))
+        for x in mycursor:
+            correct += 1
+        if correct == 0:
+            print("That password is not correct. Enter the correct password or scram!")
+
+def Show_Chompskis():
+    print("age, name, height, weight, no. of teeth, swarm_id")
+    mycursor.execute("SELECT * FROM Gnome_Chompskis")
+    for x in mycursor:
+        print(x)
+        
+def Add_Chompski(age : int, name : str, height : float, weight : float, no_teeth : int, swarm_id : int):
+    try: 
+        mycursor.execute("INSERT Gnome_Chompskis(age, name, height, weight, no_teeth, swarm_id)VALUES(%s,%s,%s,%s,%s,%s)", (age, name, height, weight, no_teeth, swarm_id))
+        db.commit()
+    except mysql.connector.IntegrityError as err:
+        print("Error: {}".format(err))
+        return err
+    
+    mycursor.execute("SELECT * FROM Gnome_Chompskis")
+    for x in mycursor:
+        print(x)
+    choice = input("Database Updated [press ENTER]")
+    if choice == 1:
+       db.commit()
+       
+def Create_Tables():
+    mycursor.execute("CREATE TABLE Employee (employee_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,fname VARCHAR(50) NOT NULL, mname VARCHAR(50),lname VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, authorization ENUM('Intern','Employee', 'Supervisor', 'Bossman'))")
+    mycursor.execute("CREATE TABLE Swarm (swarm_id int PRIMARY KEY AUTO_INCREMENT, name varchar(45) NOT NULL, latitude double(9, 5), longitude double (9,5))")
+    mycursor.execute("CREATE TABLE Oversees (employee_id int, swarm_id int, FOREIGN KEY(employee_id) REFERENCES Employee(employee_id),  FOREIGN KEY(swarm_id) REFERENCES Swarm(swarm_id))")
+    mycursor.execute("CREATE TABLE Gnome_Chompskis (chompskis_id int PRIMARY KEY AUTO_INCREMENT,name varchar(45) NOT NULL,  age smallint, height double(10,2), weight double (10,2), no_teeth int UNSIGNED, swarm_id int, FOREIGN KEY(swarm_id) REFERENCES Swarm(swarm_id))")
+    print("Tables created")
