40d7ef920195594251c8abc49627da1eb94c1c5e
James C <jrcnb4@umsystem.edu>
2024-04-27 20:45:33 -0500

    more updates

---
+++ other_main.py
@@ -0,0 +1,14 @@
+# This is just a snippet of the real code base. It will not run as is.
+import queries as sq
+import time
+import os
+
+
+
+    
+        
+def main():
+    sq.login()
+
+if __name__ == "__main__":
+    main()
