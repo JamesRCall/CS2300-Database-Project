aa02a4deda53800cddbf7bbfdc6dfdf6d6a90866
James C <jrcnb4@umsystem.edu>
2024-05-05 10:07:04 -0500

    patch 1.2 added view script for sql workshop

---
+++ view.sql
@@ -0,0 +1,26 @@
+-- Query to view all records from the Users table
+SELECT * FROM Users;
+
+-- Query to view all records from the Languages table
+SELECT * FROM Languages;
+
+-- Query to view all records from the User_Selected_Languages table
+SELECT * FROM User_Selected_Languages;
+
+-- Query to view all records from the Word table
+SELECT * FROM Word;
+
+-- Query to view all records from the Word_Definition table
+SELECT * FROM Word_Definition;
+
+-- Query to view all records from the Word_List table
+SELECT * FROM Word_List;
+
+-- Query to view all records from the Words_In_List table
+SELECT * FROM Words_In_List;
+
+-- Query to view all records from the User_Learned_Words table
+SELECT * FROM User_Learned_Words;
+
+-- Query to view all records from the Translation table
+SELECT * FROM Translation;
